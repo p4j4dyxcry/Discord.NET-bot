0e4c000d9c3c45abac20e480525292f89a07f471
ytsune <11988607+p4j4dyxcry@users.noreply.github.com>
2025-08-13 20:50:00 +0900

    Fix environment names (#14)

--- TsDiscordBot.Core/Envs.cs
+++ TsDiscordBot.Core/Envs.cs
@@ -5,6 +5,6 @@
         public static string OPENAI_API_KEY => Environment.GetEnvironmentVariable(nameof(OPENAI_API_KEY)) ?? string.Empty;
         public static string OPENAI_PROMPT => Environment.GetEnvironmentVariable(nameof(OPENAI_PROMPT)) ?? string.Empty;
         public static string DISCORD_TOKEN => Environment.GetEnvironmentVariable(nameof(DISCORD_TOKEN)) ?? string.Empty;
-        public static string LIGHTDB_PATH => Environment.GetEnvironmentVariable(nameof(LIGHTDB_PATH)) ?? string.Empty;
+        public static string LITEDB_PATH => Environment.GetEnvironmentVariable(nameof(LITEDB_PATH)) ?? string.Empty;
     }
 }
