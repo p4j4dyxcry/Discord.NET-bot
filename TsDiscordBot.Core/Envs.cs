9f94f47625e968bfa37ba0fbfb29d9396e7c6807
ytsune <at12k313@gmail.com>
2025-08-13 20:13:36 +0900

    Improve managing env values

---
+++ TsDiscordBot.Core/Envs.cs
@@ -0,0 +1,9 @@
+﻿namespace TsDiscordBot.Core
+{
+    public static class Envs
+    {
+        public static string OPENAI_API_KEY => Environment.GetEnvironmentVariable(nameof(OPENAI_API_KEY)) ?? string.Empty;
+        public static string OPENAI_PROMPT => Environment.GetEnvironmentVariable(nameof(OPENAI_PROMPT)) ?? string.Empty;
+        public static string DISCORD_TOKEN => Environment.GetEnvironmentVariable(nameof(DISCORD_TOKEN)) ?? string.Empty;
+    }
+}
