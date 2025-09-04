6c9552377c0485d4344315695b74a9b1f41299fd
ytsune <11988607+p4j4dyxcry@users.noreply.github.com>
2025-09-04 21:42:54 +0900

    feat: add server-wide anonymous profile

---
+++ TsDiscordBot.Core/Data/AnonymousGuildUserSetting.cs
@@ -0,0 +1,13 @@
+namespace TsDiscordBot.Core.Data;
+
+public class AnonymousGuildUserSetting
+{
+    public const string TableName = "anonymous_guild_user_setting";
+
+    public int Id { get; set; }
+    public ulong GuildId { get; set; }
+    public ulong UserId { get; set; }
+    public bool IsAnonymous { get; set; }
+    public string? AnonymousName { get; set; }
+    public string? AnonymousAvatarUrl { get; set; }
+}
