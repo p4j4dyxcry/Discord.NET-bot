483c149803eb885cde94fcdf29cf928c1ec0e648
ytsune <11988607+p4j4dyxcry@users.noreply.github.com>
2025-09-15 00:17:46 +0900

    Add virtual currency schema and balance command (#178)

---
+++ TsDiscordBot.Core/Amuse/ShowCashService.cs
@@ -0,0 +1,26 @@
+using System.Linq;
+using TsDiscordBot.Core.Framework;
+using TsDiscordBot.Core.Services;
+
+namespace TsDiscordBot.Core.Amuse;
+
+public class ShowCashService : IAmuseService
+{
+    private readonly DatabaseService _databaseService;
+
+    public ShowCashService(DatabaseService databaseService)
+    {
+        _databaseService = databaseService;
+    }
+
+    public Task ExecuteAsync(IMessageData message)
+    {
+        var cash = _databaseService
+            .FindAll<AmuseCash>(AmuseCash.TableName)
+            .FirstOrDefault(x => x.UserId == message.AuthorId);
+
+        var amount = cash?.Cash ?? 0;
+        return message.ReplyMessageAsync($"{message.AuthorMention}さんは現在{amount}GAL円を保持しています。");
+    }
+}
+
