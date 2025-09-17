8005fca363c75fe7a03c84280dd5c8f323519403
ytsune <at12k313@gmail.com>
2025-09-17 11:48:37 +0900

    fix name space

--- TsDiscordBot.Tests/DatabaseServiceTests.cs
+++ TsDiscordBot.Tests/DatabaseServiceTests.cs
@@ -1,11 +1,4 @@
-using System;
-using System.Collections.Generic;
-using System.IO;
 using System.Linq;
-using System.Reflection;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging.Abstractions;
-using TsDiscordBot.Core.Services;
 using Xunit;
 using Xunit.Abstractions;
 
